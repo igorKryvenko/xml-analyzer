ad16d2758f2038cab222c15742c31964da600615
Igor Kryvenko <kryvenko7igor@gmail.com>
2018-06-13 22:00:39 +0300

    Initial commit

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 It is built on top of [Jsoup](https://jsoup.org/).
 
 You can use Jsoup for your solution or apply any other convenient library. 
+
+To run the samples: <br>
+`cd samples` <br>
+`java -jar all-in-one-jar-0.0.1.jar originalFile diffFile` <br>
+where `originalFile` is an absolute path to original file, and `diffFile` is an absolute path to diff file.<br>
+E.g. `java -jar all-in-one-jar-0.0.1.jar sample-0-origin.html sample-1-evil-gemini.html`
+
--- src/main/java/com/agileengine/JsoupFindByIdSnippet.java
+++ src/main/java/com/agileengine/JsoupFindByIdSnippet.java
@@ -8,6 +8,7 @@ import java.util.stream.Collectors;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,14 +19,13 @@ public class JsoupFindByIdSnippet {
 
     private static String CHARSET_NAME = "utf8";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        // Jsoup requires an absolute file path to resolve possible relative paths in HTML,
-        // so providing InputStream through classpath resources is not a case
-        String resourcePath = "./samples/startbootstrap-freelancer-gh-pages-cut.html";
-        String targetElementId = "sendMessageButton";
+        String originalDoc = args[0];
+        String diffDoc = args[1];
+        String targetElementId = "make-everything-ok-button";
 
-        Optional<Element> buttonOpt = findElementById(new File(resourcePath), targetElementId);
+        Optional<Element> buttonOpt = findElementById(new File(originalDoc), targetElementId);
 
         Optional<String> stringifiedAttributesOpt = buttonOpt.map(button ->
                 button.attributes().asList().stream()
@@ -33,7 +33,21 @@ public class JsoupFindByIdSnippet {
                         .collect(Collectors.joining(", "))
         );
 
+
         stringifiedAttributesOpt.ifPresent(attrs -> LOGGER.info("Target element attrs: [{}]", attrs));
+
+
+        Document doc1 = Jsoup.parse(
+                new File(diffDoc),
+                CHARSET_NAME,
+                new File(diffDoc).getAbsolutePath()
+        );
+        findDiffElementById(doc1,buttonOpt.get());
+        findDiffElementByTitle(doc1,buttonOpt.get());
+        findDiffElementByClass(doc1,buttonOpt.get());
+
+
+
     }
 
     private static Optional<Element> findElementById(File htmlFile, String targetElementId) {
@@ -49,6 +63,59 @@ public class JsoupFindByIdSnippet {
             LOGGER.error("Error reading [{}] file", htmlFile.getAbsolutePath(), e);
             return Optional.empty();
         }
+    }
+
+    private static void findDiffElementById(Document diffDocument, Element originalElement) {
+        Element elementById = diffDocument.getElementById(originalElement.id());
+        if(null != elementById) {
+            LOGGER.info("Path to the matched element: {}",getXpath(elementById));
+        }
+    }
+
+    private static void findDiffElementByClass(Document diffDocuement, Element originalElement) {
+        originalElement.classNames().forEach(className -> {
+            Elements elementsByClass = diffDocuement.getElementsByClass(className);
+            if(null != elementsByClass) {
+                elementsByClass.forEach(element-> {
+                    if(element.attributes().get("title").equalsIgnoreCase(originalElement.attr("title"))) {
+                        LOGGER.info("Path to the matched by class element: {}, attrs {}", getXpath(element), getElementAttrs(element));
+                    }
+                });
+            }
+        });
+    }
+
+    private static void findDiffElementByTitle(Document diffDocument, Element originalElement) {
+        String title = originalElement.attributes().get("title");
+        if(null != title) {
+            Elements elementsByAttribute = diffDocument.getElementsByAttribute(title);
+            if(null != elementsByAttribute) {
+                elementsByAttribute.forEach(element -> {
+                    LOGGER.info("Path to the matched by title element: {}", getXpath(element));
+                });
+            }
+        }
+    }
+
+    private static String getXpath(Element htmlElement) {
+        StringBuilder absPath=new StringBuilder();
+        Elements parents = htmlElement.parents();
+
+        for (int j = parents.size()-1; j >= 0; j--) {
+            Element element = parents.get(j);
+            absPath.append("/");
+            absPath.append(element.tagName());
+            absPath.append("[");
+            absPath.append(element.siblingIndex());
+            absPath.append("]");
+        }
+        return absPath.toString();
+    }
+
+    private static String getElementAttrs(Element element) {
+        return element.attributes().asList().stream()
+                .map(attr -> attr.getKey() + " = " + attr.getValue())
+                .collect(Collectors.joining(", "));
     }
 
 }
